bd5166d1612937f21e47dff9fa24e61894fc7bbb
Bailey Behning <95610051+BaileyJBehning@users.noreply.github.com>
2022-02-08 13:29:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection.js');
 const helpers = require('./utils/helpers.js');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 const sess = {
   secret: 'Super secret secret',
